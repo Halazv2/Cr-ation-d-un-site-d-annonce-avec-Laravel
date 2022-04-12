95d9c5feeb9a77de857a046da50a259950625e8a
Halazv2 <halaziani2003@gmail.com>
2022-04-12 22:27:17 +0000

    first commit

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+
+};
+</script>
+
+<style scoped>
+</style>
