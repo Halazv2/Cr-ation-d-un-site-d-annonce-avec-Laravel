705ad0d3f0406bdcab587432b7607a321db99a13
Halazv2 <halaziani2003@gmail.com>
2022-04-14 15:13:00 +0000

    404

---
+++ frontend/src/views/PageNotFound404View.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <PageNotFound-404 />
+  </div>
+</template>
+
+<script>
+import PageNotFound404 from '../components/PageNotFound404.vue'
+export default {
+components:{ PageNotFound404 },
+name:'PageNotFound404'
+}
+</script>
+
+<style>
+
+</style>
