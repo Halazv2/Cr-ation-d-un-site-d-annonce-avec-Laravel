0424a354c2360278a11e71c7c3f9e324fb288af7
Halazv2 <halaziani2003@gmail.com>
2022-04-19 11:36:29 +0000

    add column

--- backend/database/migrations/2022_04_17_171823_create_demandes_table.php
+++ backend/database/migrations/2022_04_17_171823_create_demandes_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('demandes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")->constrained("users")->cascadeOnDelete();
             $table->string('Description');
             $table->string('Sujet');
             $table->string('image');
